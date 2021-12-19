0f28b5e5c1bc42f5aa17e3cc0470e2531fdcd7f4
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-18 16:34:20 -0800

    changes to styles, title textContent

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const discographySection = document.querySelector('#discography-section');
 
 window.addEventListener('load', async() => {
     const discography = await getDiscography();
-
+    
     for (let disc of discography) {
         const discographyEl = renderDiscography(disc);
         discographySection.append(discographyEl);
